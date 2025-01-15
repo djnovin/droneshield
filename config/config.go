4f87aa780d4b1b4e8ec061a983de10f2f767f43c
djnovin <hello@novinnoori.com>
2025-01-15 16:33:58 +1100

    bug/remove secrets

--- config/config.go
+++ config/config.go
@@ -2,23 +2,34 @@ package config
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 	"time"
 
-	_ "github.com/lib/pq" 
+	_ "github.com/lib/pq"
 )
 
 func InitDB() *sql.DB {
-  connStr := "postgresql://neondb_owner:6xB3UyVfjkON@ep-tight-union-a7q3zgwi.ap-southeast-2.aws.neon.tech/neondb?sslmode=require"
+	user := getEnv("DB_USER", "neondb_owner")
+	password := getEnv("DB_PASSWORD", "your_default_password")
+	host := getEnv("DB_HOST", "ep-tight-union-a7q3zgwi.ap-southeast-2.aws.neon.tech")
+	port := getEnv("DB_PORT", "5432")
+	dbName := getEnv("DB_NAME", "neondb")
+	sslMode := getEnv("DB_SSLMODE", "require")
+
+	connStr := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=%s",
+		user, password, host, port, dbName, sslMode)
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatalf("Error opening database: %v", err)
 	}
 
-	db.SetMaxOpenConns(10)                 // Maximum number of open connections
-	db.SetMaxIdleConns(5)                  // Maximum number of idle connections
-	db.SetConnMaxLifetime(5 * time.Minute) // Maximum connection lifetime
+	// Set connection pool parameters
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(5)
+	db.SetConnMaxLifetime(5 * time.Minute)
 
 	if err := db.Ping(); err != nil {
 		log.Fatalf("Error pinging database: %v", err)
@@ -26,4 +37,11 @@ func InitDB() *sql.DB {
 
 	log.Println("Successfully connected to database")
 	return db
+}
+
+func getEnv(key, defaultValue string) string {
+	if value, exists := os.LookupEnv(key); exists {
+		return value
+	}
+	return defaultValue
 }
